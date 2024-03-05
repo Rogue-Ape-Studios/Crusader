48a46ad6717ec020f4d7aee58177c10266c9864d
NathanThus <nathan.thus@outlook.com>
2024-03-05 14:09:55 +0100

    ✨ Create basic spawning system
    The system can now spawn enemies autonomously, within standardized parameters.

--- Assets/Scripts/Spawn/EnemySet.cs
+++ Assets/Scripts/Spawn/EnemySet.cs
@@ -1,16 +1,16 @@
 using System;
 using UnityEngine;
-using UnityEngine.AI;
+using RogueApeStudio.Crusader.HealthSystem;
 
 namespace RogueApeStudio.Crusader.Spawn
 {
     [Serializable]
     public class EnemySet
     {
-        [SerializeField] private NavMeshAgent _prefab;
+        [SerializeField] private Health _prefab;
         [SerializeField] private int _count;
 
         public int Count => _count;
-        public NavMeshAgent EnemyPrefab => _prefab;
+        public Health EnemyPrefab => _prefab;
     }
 }
---
+++ Assets/Scripts/Spawn/SpawnManager.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using RogueApeStudio.Crusader.HealthSystem;
+using UnityEngine;
+
+namespace RogueApeStudio.Crusader.Spawn
+{
+    public class SpawnManager : MonoBehaviour
+    {
+        #region Events
+        
+        public event Action OnWaveComplete;
+        public event Action OnLevelComplete;
+
+        #endregion
+
+        #region Serialized Fields
+
+        [SerializeField] private List<Transform> _spawnLocations;
+        [SerializeField] private List<Health> _enemies;
+        [SerializeField] private List<Wave> _waves;
+        [SerializeField] private Transform _waveHolder;
+        [SerializeField, Range(0, 50)] private int _minimumEnemiesThreshold = 5;
+
+        #endregion
+
+        #region Fields
+
+        private int _currentWave = 0;
+        private int _remainingEnemies;
+
+        #endregion
+
+        #region Properties
+
+        internal int RemainingEnemies
+        {
+            get
+            {
+                return _remainingEnemies;
+            }
+            private set
+            {
+                _remainingEnemies = value;
+
+                if (_remainingEnemies < _minimumEnemiesThreshold)
+                {
+                    NextWave();
+                    OnWaveComplete?.Invoke();
+                }
+            }
+        }
+
+        internal Wave CurrentWave => _waves[_currentWave];
+
+        internal Vector3 SpawnPosition => _spawnLocations[UnityEngine.Random.Range(0, _spawnLocations.Count)].position;
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Start()
+        {
+            OnWaveComplete += HandleWaveComplete;
+            OnLevelComplete += HandleLevelComplete;
+            RemainingEnemies = 0;
+        }
+
+        private void OnDestroy()
+        {
+            OnWaveComplete -= HandleWaveComplete;
+        }
+
+        #endregion
+
+        #region Internal
+
+        internal async void NextWave()
+        {
+            if (_currentWave >= _waves.Count)
+            {
+                OnLevelComplete?.Invoke();
+                return;
+            }
+
+            await StandardizedSpawnsAsync();
+        }
+
+
+        #endregion
+
+        #region Private
+
+        private async UniTask StandardizedSpawnsAsync()
+        {
+            foreach (var enemySet in _waves[_currentWave].Enemies)
+            {
+                for (var i = 0; i < enemySet.Count; i++)
+                {
+                    Debug.Log("Spawning!");
+                    var enemy = Instantiate(enemySet.EnemyPrefab,
+                                            SpawnPosition,
+                                            Quaternion.identity,
+                                            _waveHolder);
+
+                    enemy.OnDeath += HandleEnemyDeath;
+                    enemy.enabled = true;
+                }
+                await UniTask.WaitForSeconds(_waves[_currentWave].TimeBetweenSpawns);
+            }
+        }
+
+        private void Disable()
+        {
+            enabled = false;
+        }
+
+        private void HandleEnemyDeath()
+        {
+            RemainingEnemies--;
+        }
+
+        private void HandleWaveComplete()
+        {
+            _waves.RemoveAt(_currentWave);
+            NextWave();
+        }
+
+        private void HandleLevelComplete()
+        {
+            Debug.Log("Level Completed!");
+            Disable();
+        }
+
+        #endregion
+    }
+}
