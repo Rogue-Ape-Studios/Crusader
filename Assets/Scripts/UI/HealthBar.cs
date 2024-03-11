eba11136b205b6b462af113b569c364d67009651
davey2206 <davey2206@live.nl>
2024-03-11 11:54:50 +0100

    ✨Added Health Bar Functionality

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RogueApeStudio.Crusader.HealthSystem;
+
+namespace RogueApeStudio.Crusader.UI.Healthbar
+{
+    public class HealthBar : MonoBehaviour
+    {
+        private float _barMaxWidth;
+        private float _barWidth;
+        private float _velocity;
+
+        [SerializeField] private RectTransform _bar;
+        [SerializeField] private Health _player;
+
+        private void Start()
+        {
+            _barMaxWidth = _bar.sizeDelta.x;
+        }
+
+        private void Update()
+        {
+            _barWidth = (_barMaxWidth / _player.MaxHealth) * _player.CurrentHealth;
+
+            _bar.sizeDelta = new Vector2(Mathf.SmoothDamp(_bar.sizeDelta.x, _barWidth, ref _velocity, 25 * Time.deltaTime), _bar.sizeDelta.y);
+        }
+    }
+}
