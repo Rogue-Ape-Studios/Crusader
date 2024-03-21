fcdb7777c48f3feba9faec3f4df271ec2cb6b3a5
davey2206 <davey2206@live.nl>
2024-03-21 12:06:47 +0100

    ✨Added basic attack vfx

--- Assets/Scripts/Player/Combat/BasicAttack.cs
+++ Assets/Scripts/Player/Combat/BasicAttack.cs
@@ -23,6 +23,7 @@ namespace RogueApeStudio.Crusader.Player.Combat
         [SerializeField] private Animator _animator;
         [SerializeField] private AnimationClip[] _animations;
         [SerializeField] private Collider _sword;
+        [SerializeField] private GameObject _vfx;
 
         [Header("Attack Info")]
         [SerializeField] private int _comboCounter = 0;
@@ -77,6 +78,7 @@ namespace RogueApeStudio.Crusader.Player.Combat
         private void Attack()
         {
             _sword.enabled = true;
+            _vfx.SetActive(true);
             _animator.Play(_animations[_comboCounter - 1].name);
             _delay = _animations[_comboCounter - 1].length / _attackSpeed;
             _attackWindow = 0.5f;
@@ -111,6 +113,7 @@ namespace RogueApeStudio.Crusader.Player.Combat
                 await UniTask.WaitForSeconds(_delay, cancellationToken: token);
                 _rb.velocity = Vector3.zero;
                 _sword.enabled = false;
+                _vfx.SetActive(false);
                 _canAttack = true;
             }
             catch (OperationCanceledException)
