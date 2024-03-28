dcbb39736abc3f9348a46379478e62950fb0d0f8
NathanThus <nathan.thus@outlook.com>
2024-03-28 12:21:04 +0100

    🍱 Create TakeDamageItem
    Will be the item template for any effect that occurs when the player takes damage.

---
+++ Assets/Scripts/Items/Conditional/TakeDamageItem.cs
@@ -0,0 +1,29 @@
+using System;
+using RogueApeStudio.Crusader.HealthSystem;
+
+namespace RogueApeStudio.Crusader.Items.Conditional
+{
+    internal class TakeDamageItem : ConditionalItem
+    {
+        internal override void SetupCondition()
+        {
+            if(_conditionTarget == null)
+            {
+                throw new ArgumentNullException(nameof(_conditionTarget));
+            }
+
+            var health = _conditionTarget as Health;
+            health.OnDamage += HandleConditionMet;
+        }
+
+        private void HandleConditionMet(float _)
+        {
+            base.HandleConditionMet();
+        }
+
+        internal override void ConditionalItemEffect()
+        {
+            
+        }
+    }
+}
