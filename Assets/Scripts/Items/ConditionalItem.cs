8f6541c10bfcf7398295fed2f1302aab0dfffb75
NathanThus <nathan.thus@outlook.com>
2024-03-28 12:14:47 +0100

    🎨 Add proper accessors to ConditonalItem

--- Assets/Scripts/Items/ConditionalItem.cs
+++ Assets/Scripts/Items/ConditionalItem.cs
@@ -7,13 +7,13 @@ namespace RogueApeStudio.Crusader.Items
     public abstract class ConditionalItem : MonoBehaviour
     {
         [Header("Conditon Settings")]
-        [SerializeField, Tooltip("Will be cast to the correct value")] MonoBehaviour _conditionTarget;
+        [SerializeField, Tooltip("Will be cast to the correct value")] protected MonoBehaviour _conditionTarget;
         [Header("Visuals Effects Settings")]
-        [SerializeField] VisualEffect _itemVisualEffect;
+        [SerializeField] protected VisualEffect _itemVisualEffect;
         [Header("Sound settings")]
-        [SerializeField] AudioClip _audioClip;
-        [SerializeField] Transform _soundTarget;
-        [SerializeField] float _volume = 1f;
+        [SerializeField] protected AudioClip _audioClip;
+        [SerializeField] protected Transform _soundTarget;
+        [SerializeField] protected float _volume = 1f;
 
         /// <summary>
         /// Sets up the condition to be invoked. Cast the _conditonTarget and subscribe to the correct event here.
